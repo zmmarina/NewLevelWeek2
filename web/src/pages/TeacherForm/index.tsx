8c092dd9c6d6c2a330a3d97fef05d653b125331b
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-08-06 17:30:15 -0300

    working on inputs

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
+import Textarea from "../../components/Textarea";
+import Select from "../../components/Select";
 
 import warningIcon from "../../assets/images/icons/warning.svg" 
 
@@ -23,15 +25,53 @@ function TeacherForm(){
                     <Input name="name" label="Full name"/>
                     <Input name="avatar" label="Avatar"/>
                     <Input name="whatsapp" label="WhatsApp"/>
+                    <Textarea name="bio" label="About you" />
 
                 </fieldset>
 
                 <fieldset>
                     <legend>About the class</legend>
 
-                    <Input name="subject" label="Language"/>
+                    <Select 
+                    name="subject" 
+                    label="Language"
+                    options={[
+                        { value: "English", label: "English"},
+                        { value: "French", label: "French"},
+                        { value: "German", label: "German"},
+                        { value: "Japanese", label: "Japanese"},                  
+                        { value: "Spanish", label: "Spanish"},
+                    ]}
+                    />
                     <Input name="cost" label="Cost per hour"/>
                   
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Available schedule
+                        <button type="button">
+                            + New schedule
+                        </button>
+                    </legend>
+
+                    <div className="schedule-item">
+                        <Select
+                        name="week_day" 
+                        label="Day of the week"
+                        options={[
+                            { value: "0", label: "Sunday"},                       
+                            { value: "1", label: "Monday"},
+                            { value: "2", label: "Tuesday"},
+                            { value: "3", label: "Wednesday"},
+                            { value: "4", label: "Thursday"},                  
+                            { value: "5", label: "Friday"},
+                            { value: "6", label: "Saturday"},
+                        ]}
+                        />
+                        <Input name="from" label="from" type="time" />
+                        <Input name="to" label="to" type="time"/>                       
+                    </div>
                 </fieldset>
 
                 <footer>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 import "./styles.css";
 
@@ -12,9 +13,31 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="These are the available teachers">
                 <form id="search-teachers">
-                   <Input name="subject" label="Language"/>
-                   <Input name="week_day" label="Day of the week" />
-                   <Input type="time" name="time" label="Time" />                                       
+                <Select
+                    name="subject" 
+                    label="Language"
+                    options={[
+                        { value: "English", label: "English"},
+                        { value: "French", label: "French"},
+                        { value: "German", label: "German"},
+                        { value: "Japanese", label: "Japanese"},                  
+                        { value: "Spanish", label: "Spanish"},
+                    ]}
+                    />
+                    <Select
+                    name="week_day" 
+                    label="Day of the week"
+                    options={[
+                        { value: "0", label: "Sunday"},                       
+                        { value: "1", label: "Monday"},
+                        { value: "2", label: "Tuesday"},
+                        { value: "3", label: "Wednesday"},
+                        { value: "4", label: "Thursday"},                  
+                        { value: "5", label: "Friday"},
+                        { value: "6", label: "Saturday"},
+                    ]}
+                    />
+                    <Input type="time" name="time" label="Time" />                                       
                 </form>
             </PageHeader>
 
